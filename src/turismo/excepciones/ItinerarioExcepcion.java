e7cedac3c04472877491db5bb5c15c06e4253477
IgnaColla <ignaciocollazo98@hotmail.com>
2023-06-12 19:50:22 -0300

    Delete de Usuario, start de ItinerarioTest

---
+++ src/turismo/excepciones/ItinerarioExcepcion.java
@@ -0,0 +1,10 @@
+package turismo.excepciones;
+
+public class ItinerarioExcepcion extends Exception {
+	
+	private static final long serialVersionUID = 6569226179521544322L;
+
+	public ItinerarioExcepcion(String message) {
+		super(message);
+	}
+}
