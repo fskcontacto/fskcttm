6e92810631f1dcae69bb6aad9ec8d8908bda9698
JuanCruzLoio <jloiotile@provinciamicrocreditos.com>
2023-06-12 21:07:10 -0300

    desarrollo ItinerarioTest

--- test/turismo/test/ItinerarioTest.java
+++ test/turismo/test/ItinerarioTest.java
@@ -1,32 +1,48 @@
 package turismo.test;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import turismo.sistema.Atraccion;
+import turismo.sistema.Itinerario;
+import turismo.sistema.Sugerencia;
+
 public class ItinerarioTest {
-//	private Itinerario itinerario;
-//	private double costo;
-//	private double duracion;
-////	private List<Sugerencia> sugAceptadas = new ArrayList<Sugerencia>();
-
-//	this.sugAceptadas.add("Erebor","Paisaje",12,2);
-
+	private Itinerario itinerario;
+	private double costo=10;
+	private double duracion=2;
+	private List<Sugerencia> sugAceptadas = new ArrayList<Sugerencia>();
+	private Atraccion atraccion;
+	
+	
+	
 	@Before
 	public void setUp() throws Exception {
-//		itinerario = new Itinerario(12, 2, sugAceptadas);
+		atraccion = new Atraccion("Erebor", "Paisaje", costo, duracion, 6);
+		itinerario=new Itinerario();
+		itinerario.agregarSugerencia(atraccion);
+		sugAceptadas.add(atraccion);
 	}
 
 	@Test
 	public void QueObtengaCosto() {
-
+		Assert.assertEquals(costo, itinerario.getCostoItinerario(),0.1);
 	}
-
+	
 	@Test
 	public void QueObtengaDuracion() {
+		Assert.assertEquals(duracion, itinerario.getDuracionItinerario(),0.1);
 	}
-
+	
 	@Test
 	public void QueObtengaSugerenciasAceptadas() {
+		Assert.assertEquals(sugAceptadas, itinerario.getItinerario());
 	}
 
 }
